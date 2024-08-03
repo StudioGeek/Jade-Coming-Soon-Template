8fad388c04e4591000645c8988d8941f29142f04
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2024-08-02 20:02:33 -0600

    :construction: Progress bar add it

--- src/react/Counter.tsx
+++ src/react/Counter.tsx
@@ -2,13 +2,14 @@ import {DateTime} from "luxon";
 
 export default function Counter({date} : {date: string}){
     const luxonDate = DateTime.fromFormat( date, 'yyyy-MM-dd');
+    
     return <>
         <div className="counter flex justify-center my-10">
             <div className={'mx-2'}>
                 <div className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
                     14
                 </div>
-                <div className={'text-sm mt-3 font-thin'}>
+                <div className={'lato-thin text-sm mt-3 font-thin'}>
                     days
                 </div>
             </div>
@@ -17,7 +18,7 @@ export default function Counter({date} : {date: string}){
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
                     23
                 </div>
-                <div className={'text-sm mt-3 font-thin'}>
+                <div className={'lato-thin text-sm mt-3 font-thin'}>
                     hours
                 </div>
             </div>
@@ -26,7 +27,7 @@ export default function Counter({date} : {date: string}){
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
                     23
                 </div>
-                <div className={'text-sm mt-3 font-thin'}>
+                <div className={'lato-thin text-sm mt-3 font-thin'}>
                     minutes
                 </div>
             </div>
@@ -35,7 +36,7 @@ export default function Counter({date} : {date: string}){
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
                     24
                 </div>
-                <div className={'text-sm mt-3 font-thin'}>
+                <div className={'lato-thin text-sm mt-3 font-thin'}>
                     seconds
                 </div>
             </div>
---
+++ src/react/ProgressBar.tsx
@@ -0,0 +1,15 @@
+
+export default function ProgressBar({percentage}: {percentage: number}){
+    return <>
+        <div className={'w-100 flex justify-center'}>
+            <div className="progress-bar my-3 w-[calc(100%-3rem)] rounded-md shadow-semantic">
+                <div className="progress-bar-shadow bg-semantic/50 relative w-[100%] h-[2em] overflow-hidden rounded-sm flex ">
+                    <div className={`inline-block text-primary h-[2em] bg-primary`} style={{width: `${percentage}%`}}></div>
+                    <div className={`lato-bold absolute text-center top-0 w-[100%] mt-1 shadow-semantic text-white [text-shadow:_0_0_10px_rgb(0_0_0_/_40%)]`} >
+                        {percentage}%
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+}
