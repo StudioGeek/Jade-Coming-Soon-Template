9fcb5ce90b8f385510de77e155e7923a474b3749
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2024-08-26 11:44:27 -0600

    Adding algorithm for show stars

--- src/react/Counter.tsx
+++ src/react/Counter.tsx
@@ -4,7 +4,7 @@ export default function Counter({date} : {date: string}){
     const luxonDate = DateTime.fromFormat( date, 'yyyy-MM-dd');
     
     return <>
-        <div className="counter flex justify-center my-10">
+        <div className="counter flex justify-center my-10 z-10">
             <div className={'mx-2'}>
                 <div className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
                     14
--- src/react/ProgressBar.tsx
+++ src/react/ProgressBar.tsx
@@ -1,7 +1,7 @@
 
 export default function ProgressBar({percentage}: {percentage: number}){
     return <>
-        <div className={'w-100 flex justify-center'}>
+        <div className={'w-100 flex justify-center z-10'}>
             <div className="progress-bar my-3 w-[calc(100%-3rem)] rounded-md shadow-semantic">
                 <div className="progress-bar-shadow bg-semantic/50 relative w-[100%] h-[2em] overflow-hidden rounded-sm flex ">
                     <div className={`inline-block text-primary h-[2em] bg-primary`} style={{width: `${percentage}%`}}></div>
