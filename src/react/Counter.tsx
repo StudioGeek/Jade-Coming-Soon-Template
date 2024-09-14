7f8f9b8c36b3214e695369d75af54093eb3d67a6
Ivan Alvarado <ivan.alvarado@serprogramador.es>
2024-09-14 09:05:11 -0600

    :sparkles: Adding counter code, working very well

--- src/react/Counter.tsx
+++ src/react/Counter.tsx
@@ -1,13 +1,25 @@
 import {DateTime} from "luxon";
+import {useEffect, useState} from "react";
 
 export default function Counter({date} : {date: string}){
     const luxonDate = DateTime.fromFormat( date, 'yyyy-MM-dd');
+    const today = DateTime.now();
+    let diff = luxonDate.diff(today, ['days','hours','minutes','seconds']);
+    let [difference, setDifference] = useState(diff.toObject());
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const today = DateTime.now();
+            setDifference(luxonDate.diff(today, ['days','hours','minutes','seconds']).toObject());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
     
     return <>
         <div className="counter flex justify-center my-10 z-10">
             <div className={'mx-2'}>
                 <div className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
-                    14
+                    {difference.days ?? 0}
                 </div>
                 <div className={'lato-thin text-sm mt-3 font-thin'}>
                     days
@@ -16,7 +28,7 @@ export default function Counter({date} : {date: string}){
             <div className={'mx-2'}>
                 <div
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
-                    23
+                    {difference.hours ?? 0}
                 </div>
                 <div className={'lato-thin text-sm mt-3 font-thin'}>
                     hours
@@ -25,7 +37,7 @@ export default function Counter({date} : {date: string}){
             <div className={'mx-2'}>
                 <div
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
-                    23
+                    {difference.minutes ?? 0}
                 </div>
                 <div className={'lato-thin text-sm mt-3 font-thin'}>
                     minutes
@@ -34,7 +46,7 @@ export default function Counter({date} : {date: string}){
             <div className={'mx-2'}>
                 <div
                     className={'bg-semantic text-primary w-[60px] h-[60px] flex flex-col justify-center font-bold text-2xl rounded-sm'}>
-                    24
+                    {Math.round(difference.seconds!) ?? 0}
                 </div>
                 <div className={'lato-thin text-sm mt-3 font-thin'}>
                     seconds
